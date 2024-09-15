d08c2bd40d44cbac8b148fce88a6372948f777e3
khushi upadhyay <khushiupadhyay2004@gmail.com>
2024-09-16 01:15:40 +0530

    Integrated Aceternity UI, made about, exprience and projects pages, content updation pending

---
+++ src/components/pages/About.tsx
@@ -0,0 +1,54 @@
+// "use client";
+// import React from "react";
+// import { WavyBackground } from "../ui/wavy-background";
+
+// export function About() {
+//   return (
+//     <WavyBackground className="max-w-4xl mx-auto pb-40">
+//       <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+//         Hero waves are cool
+//       </p>
+//       <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
+//         Leverage the power of canvas to create a beautiful hero section
+//       </p>
+//     </WavyBackground>
+//   );
+// }
+
+
+"use client";
+import React from "react";
+import { WavyBackground } from "../ui/wavy-background";
+import { TypewriterEffect } from "../ui/typewriter-effect";
+
+export function About() {
+    const words = [
+        {
+          text: "Build",
+        },
+        {
+          text: "awesome",
+        },
+        {
+          text: "apps",
+        },
+        {
+          text: "with",
+        },
+        {
+          text: "Aceternity.",
+          className: "text-blue-500 dark:text-blue-500",
+        },
+      ];
+  return (
+    <WavyBackground className="max-w-4xl mx-auto pb-40">
+      {/* <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+        Hero waves are cool
+      </p>
+      <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
+        Leverage the power of canvas to create a beautiful hero section
+      </p> */}
+       <TypewriterEffect words={words} />
+    </WavyBackground>
+  );
+}
