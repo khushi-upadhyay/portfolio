2e6fdb4ebdea4c381403fc09a6a780b4c3e2281c
khushi upadhyay <khushiupadhyay2004@gmail.com>
2024-09-16 22:38:58 +0530

    Integrated magic-ui componentns, created skills page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import { About } from "@/components/pages/About"
+import { Skills } from "@/components/pages/Skills"
 import { Projects } from "@/components/pages/Projects"
-// import { Temp } from "@/components/pages/Temp"
+//  import { Connect } from "@/components/pages/Temp" 
 import { Experience } from "@/components/pages/Experience"
 
 export default function Home() {
   return (
     <div className="relative min-h-screen bg-black">
       <About />
+      <Skills/>
       <Projects />
+     
       <Experience />
-      {/* <Temp /> */}
     </div>
   );
 }
